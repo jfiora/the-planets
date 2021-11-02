758ab5a41b7a09f5b980d2549d3ec5a008fec931
Mauricio Garavaglia <garavagliamauricio@gmail.com>
2021-11-02 12:04:42 -0300

    router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { Switch, Link, Route } from 'react-router-dom';
+import { routes } from './routes';
+
 function App() {
-  return <div>The Solar System</div>;
+  return (
+    <div>
+      <nav>
+        <li>
+          <Link to={routes.mercury}>mercury</Link>
+        </li>
+      </nav>
+      <Switch>
+        <Route path={routes.mercury}>Mercury</Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
