09edea5a48097925b8a018d147589bf85b0b8100
JoaquinBruhnIntive <joaquin.bruhn@intive.com>
2021-11-02 18:36:06 -0300

    fixing the PlanetStats component PR

---
+++ src/components/Planet/PlanetStats/PlanetStats.jsx
@@ -0,0 +1,21 @@
+import PlanetStatItems from './PlanetStatsItem/PlanetStatItem';
+import './PlanetStats.css';
+
+const PlanetStats = ({ stats }) => {
+  return (
+    <div className="PlanetStat">
+      <PlanetStatItems
+        statType="rotation time"
+        statValue={stats.rotationTime}
+      />
+      <PlanetStatItems
+        statType="revolution time"
+        statValue={stats.revolutionTime}
+      />
+      <PlanetStatItems statType="radius" statValue={stats.radius} />
+      <PlanetStatItems statType="average temp." statValue={stats.averageTemp} />
+    </div>
+  );
+};
+
+export default PlanetStats;
