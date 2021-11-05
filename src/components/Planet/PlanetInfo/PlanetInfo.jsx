9411fa664a13c0fb63f5c73f85d86ead1f11353c
Mauricio Garavaglia <garavagliamauricio@gmail.com>
2021-11-05 16:27:43 -0300

    PlanetInfoController mobile adaptation

--- src/components/Planet/PlanetInfo/PlanetInfo.jsx
+++ src/components/Planet/PlanetInfo/PlanetInfo.jsx
@@ -1,39 +1,43 @@
-import React, { useState, useEffect } from "react";
-import PlanetDescription from "./PlanetDescription/PlanetDescription";
-import PlanetImage from "./PlanetImage/PlanetImage";
-import PlanetInfoController from "./PlanetInfoController/PlanetInfoController";
+import React, { useState, useEffect } from 'react';
+import './PlanetInfo.css';
 
-const PlanetInfo = ({planet}) => {
-    const [data, setData] = useState(planet.overview);
-    const [currentPlanetInfo, setCurrentPlanetInfo] = useState("OVERVIEW");
+import PlanetDescription from './PlanetDescription/PlanetDescription';
+import PlanetImage from './PlanetImage/PlanetImage';
+import PlanetInfoController from './PlanetInfoController/PlanetInfoController';
 
-    function handlePLanetInfoChange(planetInfo) {
-        setCurrentPlanetInfo(planetInfo);
+const PlanetInfo = ({ planet }) => {
+  const [data, setData] = useState(planet.overview);
+  const [currentPlanetInfo, setCurrentPlanetInfo] = useState('OVERVIEW');
+
+  function handlePLanetInfoChange(planetInfo) {
+    setCurrentPlanetInfo(planetInfo);
+  }
+
+  function setPlanetInfo() {
+    if (currentPlanetInfo == 'OVERVIEW') {
+      setData(planet.overview);
+    } else if (currentPlanetInfo == 'INTERNAL STRUCTURE') {
+      setData(planet.structure);
+    } else {
+      setData(planet.surface);
     }
+  }
 
-    function setPlanetInfo() {
-        if (currentPlanetInfo == "OVERVIEW") {
-            setData(planet.overview);
-        } else if (currentPlanetInfo == "INTERNAL STRUCTURE") {
-            setData(planet.structure);
-        } else {
-            setData(planet.surface);
-        }
-    }
+  useEffect(() => {
+    setPlanetInfo();
+  }, [currentPlanetInfo, planet]);
 
-    useEffect(() => {
-        setPlanetInfo();
-    }, [currentPlanetInfo, planet]);
-
-    return (
-        <div>
-            <PlanetImage image={data.image} />
-            <div className="PlanetInfoWrapper">
-                <PlanetDescription title={planet.name} props={data} />
-                <PlanetInfoController sendDataToParent={handlePLanetInfoChange} currentPlanetInfo={currentPlanetInfo} planetColor={planet.color} />
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="PlanetInfo">
+      <PlanetInfoController
+        sendDataToParent={handlePLanetInfoChange}
+        currentPlanetInfo={currentPlanetInfo}
+        planetColor={planet.color}
+      />
+      <PlanetImage image={data.image} />
+      <PlanetDescription title={planet.name} props={data} />
+    </div>
+  );
+};
 
 export default PlanetInfo;
